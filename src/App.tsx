15e545390b675e2d16d2b3b198ba5c22a25ac51e
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-02-24 12:28:06 +0100

    Initial React + Vite + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-yellow-50">
+      <h1 className="text-4xl font-bold text-blue-800">Hello, ResumeEnow!</h1>
+    </div>
+  )
+}
