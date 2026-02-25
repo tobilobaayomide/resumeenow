e599e56aad77989e50d73e49e4dc0edf36ee4c83
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-02-25 23:30:44 +0100

    feat: restructure App component to use React Router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
+import MyResumes from "./components/dashboard/MyResumes";
+import Templates from "./components/dashboard/Templates";
+import CareerProfile from "./components/dashboard/CareerProfile";
+import Settings from "./components/dashboard/Settings";
+import BuilderPage from "./components/builder/BuilderPage";
+
 export default function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-yellow-50">
-      <h1 className="text-4xl font-bold text-blue-800">Hello, ResumeEnow!</h1>
-    </div>
-  )
+    <Router>
+              <Toaster position="top-center" richColors />
+
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/myresumes" element={<MyResumes />} />
+        <Route path="/dashboard/templates" element={<Templates />} />
+        <Route path="/dashboard/profile" element={<CareerProfile />} />
+        <Route path="/dashboard/settings" element={<Settings />} />
+        {/* Builder page now accepts an ID */}
+        <Route path="/builder/:id" element={<BuilderPage />} />
+      </Routes>
+    </Router>
+  );
 }
