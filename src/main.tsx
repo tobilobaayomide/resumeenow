44bb856b197ee780bd38617a3f7e914afafdd862
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-03-02 21:56:15 +0100

    feat: wrap App component with PlanProvider for improved context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext'
+import { PlanProvider } from './context/PlanContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-    <App />
+      <PlanProvider>
+        <App />
+      </PlanProvider>
     </AuthProvider>
   </StrictMode>,
 )
