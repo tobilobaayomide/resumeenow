f4cc1919b87f26cfe9fae53f118012fc938d1a88
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-03-02 22:14:39 +0100

    feat: add template gallery components for dashboard

---
+++ src/components/dashboard/template-gallery/index.ts
@@ -0,0 +1,5 @@
+export { default as TemplateCardPreview } from './TemplateCardPreview';
+export { default as TemplatesCategoryFilters } from './TemplatesCategoryFilters';
+export { default as TemplatesGallery } from './TemplatesGallery';
+export { default as TemplatesHeader } from './TemplatesHeader';
+export { default as TemplatesPreviewModal } from './TemplatesPreviewModal';
