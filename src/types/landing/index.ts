cdebbf2dc028831d1c2e09bd97bd8b4d02bf25ef
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-03-02 22:38:49 +0100

    feat: create index file to export types from landing module

---
+++ src/types/landing/index.ts
@@ -0,0 +1,7 @@
+export * from './hero';
+export * from './features';
+export * from './steps';
+export * from './navbar';
+export * from './trusted';
+export * from './footer';
+export * from './components';
