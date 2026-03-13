114cced3b716984deaa6afa8ce883fe99a7dd576
Tobiloba Ayomide <oyetunjitobiloba82@gmail.com>
2026-03-14 00:58:11 +0100

    style: tweak resume card preview scaling on md

--- src/components/dashboard/view/ResumeCardPreview.tsx
+++ src/components/dashboard/view/ResumeCardPreview.tsx
@@ -14,7 +14,7 @@ const ResumeCardPreview: React.FC<ResumeCardPreviewProps> = ({ resume }) => {
           <div className="w-full h-full border border-gray-200 rounded-md bg-gray-50" />
         </div>
       }
-      scaleWrapperClassName="absolute left-1/2 top-1 -translate-x-1/2 origin-top scale-[0.47] md:scale-[0.28] pointer-events-none"
+      scaleWrapperClassName="absolute left-1/2 top-1 -translate-x-1/2 origin-top scale-[0.47] md:scale-[0.3] pointer-events-none"
       paperClassName="w-198.5 h-280.75 bg-white border border-gray-200 shadow-sm p-9 overflow-hidden"
     />
   );
